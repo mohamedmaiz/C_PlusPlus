a9028461ea190c5711bc792a1136d37135b15bdc
maiz mohamed <mohamedmaiz9710@gmail.com>
2023-12-31 10:38:40 -0500

    Task: two dimensional arrays with random number

--- C_plusPlus/main.cpp
+++ C_plusPlus/main.cpp
@@ -1,17 +1,54 @@
 
 #include <iostream>
-#include <fstream>
+#include <vector>
+#include <iomanip>
 using namespace  std;
+
+int ReadNumber(string message) {
+	int number;
+	cout << message << endl;
+	cin >> number;
+	return number;
+}
+int ReadRandomNumber(int from, int to) {
+	return from + (rand() % to);
+}
+void ReadArrayEllement(vector<vector<int>> &array, int lengthOfCols, int lengthOfRows) {
+	for (int cols = 0; cols < lengthOfCols; cols++) {
+		for (int row = 0; row < lengthOfRows; row++) {
+			array[cols][row] = ReadRandomNumber(0, 100);
+		}
+	}
+
+}
+\
+void PrintArraysEllements(vector<vector<int>> array, int lengthOfCols, int lengthOfRows) {
+	int index = 1;
+	for (vector<int>& row : array) {
+		cout << "Array " << index << " : ";
+
+		for (int& value : row) {
+			cout << setw(3) << value << " ";
+
+		}
+			cout << endl;
+			index++;
+	}
+}
+
+
+
 
 int main()
 {
-	fstream MyFile;
-	MyFile.open("myFile.txt", ios::out | ios::app );
-	if (MyFile.is_open()) {
+	srand((unsigned)time(NULL));
+	 int arrayCols = ReadNumber("Enter the number of array cols");
+	 int arrayRows = ReadNumber("Enter the number of array rows");
+	 vector<vector<int>> array ;
+	 array.resize(arrayCols, vector<int>(arrayRows));
+	ReadArrayEllement(array, arrayCols, arrayRows);
+	PrintArraysEllements(array, arrayCols, arrayRows);
 
-		MyFile << "My Fist Line in my file using c++ \n";
-		MyFile.close();
-	}
 	return 0;
 }
 
