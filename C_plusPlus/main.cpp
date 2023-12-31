69e6e26eaf1dbb118607a1501dd2003f4c296472
maiz mohamed <mohamedmaiz9710@gmail.com>
2023-12-31 13:02:04 -0500

    Task: calculate sum of each cols in vector

--- C_plusPlus/main.cpp
+++ C_plusPlus/main.cpp
@@ -41,18 +41,32 @@ int RowSum(vector<int> row) {
 	}
 	return sum;
 }
+int colSum(vector<vector<int>> vec, int colNumber , int length) {
+	int sum = 0;
+	for (int i = 0; i < length; i++) {
+		sum += vec[i][colNumber];
+	}
+	return sum;
+}
 void SumRowsInVector(vector<vector<int>> vec , vector<int> &vecOfSums) {
 	for (vector<int>& row : vec) {
 		vecOfSums.push_back(RowSum(row));
 	}
 }
-
-void PrintVector(vector<int> vec) {
+void PrintVector(vector<int> vec , string type) {
+	int index = 1;
 	for (int& value : vec) {
-		cout << "Row 1 Sum = " << value << endl;
+		cout << type << index << " Sum = " << value << endl;
+		index++;
 	}
 
 }
+void SumColsInVector(vector<vector<int>> vec, vector<int>& vecOfSums  , int lengthOfCols, int lengthOfRows) {
+	for (int row = 0; row < lengthOfRows; row++) {
+		vecOfSums.push_back(colSum(vec, row, lengthOfCols));
+		}
+}
+
 
 
 
@@ -61,16 +75,21 @@ void PrintVector(vector<int> vec) {
 int main()
 {
 	srand((unsigned)time(NULL));
-	 int arrayCols = ReadNumber("Enter the number of array cols");
-	 int arrayRows = ReadNumber("Enter the number of array rows");
-	 vector<vector<int>> vec ;
-	 vector<int> sumOfEachRow;
-	 vec.resize(arrayCols, vector<int>(arrayRows));
+	int arrayCols = ReadNumber("Enter the number of array cols");
+	int arrayRows = ReadNumber("Enter the number of array rows");
+	vector<vector<int>> vec ;
+	vector<int> sumOfEachRow;
+	vector<int> sumOfEachcol;
+	vec.resize(arrayCols, vector<int>(arrayRows));
 	ReadMatrixVectorEllement(vec, arrayCols, arrayRows);
 	PrintMatrixVectorEllements(vec);
-	PrintVector(sumOfEachRow);
 	SumRowsInVector(vec, sumOfEachRow);
-	PrintVector(sumOfEachRow);
+	SumColsInVector(vec, sumOfEachcol , arrayCols , arrayRows);
+	cout << "\nsum of rows" << endl;
+	PrintVector(sumOfEachRow , "Row ");
+	cout << "\nsum of cols" << endl;
+	PrintVector(sumOfEachcol , "Col ");
+
 	return 0;
 }
 
